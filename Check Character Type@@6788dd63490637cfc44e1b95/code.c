eaa2b00d5c672e674cfdec9b8933e798879a4181
manastaneja13 <manastaneja1303@gmail.com>
2025-02-13 23:01:55 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -8,7 +8,7 @@ int main(){
     else if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
         printf("Vowel");
     }
-    else if(ch>='a'&&ch<='z'&&ch>='A'&&ch<='Z'){
+    else if(ch>='a'&&ch<='z'||ch>='A'&&ch<='Z'){
         printf("Consonant");
     }
     else{
